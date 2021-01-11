78c9a5431c381b1068d75f6331c6a5e02069efde
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-11 11:23:56 -0300

    Finalização das configurações e desenvolvimento...

--- app/src/main/java/com/cursoandroid/snackbar/MainActivity.java
+++ app/src/main/java/com/cursoandroid/snackbar/MainActivity.java
@@ -3,12 +3,50 @@ package com.cursoandroid.snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+    private Snackbar snackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                snackbar = Snackbar.make(
+                view,"Botão pressionado", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Confirmar", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    buttonAbrir.setText("Botão abrir alterado");
+
+                    }
+                });
+
+                snackbar.setActionTextColor(getResources().getColor(R.color.colorPrimary));
+                snackbar.show();
+
+            }
+        });
+
+
     }
+
+    public void abrirSnackbar(View view){
+
+    }
+
+
 }
